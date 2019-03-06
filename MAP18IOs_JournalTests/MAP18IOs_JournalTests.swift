61aef07e7647692c42863bcd712560e7048d90cd
David Svensson <david.svensson@nackademin.se>
2019-03-06 09:59:16 +0100

    First commit

---
+++ MAP18IOs_JournalTests/MAP18IOs_JournalTests.swift
@@ -0,0 +1,34 @@
+//
+//  MAP18IOs_JournalTests.swift
+//  MAP18IOs_JournalTests
+//
+//  Created by David Svensson on 2019-03-06.
+//  Copyright © 2019 David Svensson. All rights reserved.
+//
+
+import XCTest
+@testable import MAP18IOs_Journal
+
+class MAP18IOs_JournalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
