4c6870b24c881d7342f82f7244d504372997c391
David Svensson <david.svensson@nackademin.se>
2019-03-06 11:06:07 +0100

    journalEntry class

---
+++ MAP18IOs_Journal/JournalEntry.swift
@@ -0,0 +1,9 @@
+//
+//  JournalEntry.swift
+//  MAP18IOs_Journal
+//
+//  Created by David Svensson on 2019-03-06.
+//  Copyright © 2019 David Svensson. All rights reserved.
+//
+
+import Foundation
